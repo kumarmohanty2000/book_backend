6d84438eca06660e5ae47f0a1d06cf1b43b4eb6d
SAROJ KUMAR MOHANTY <kumarmohantysaroj008@gmail.com>
2024-11-21 23:16:34 +0530

    "/" added

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
+  
+app.use('/',(req,res)=> res.json("hi how are you"))
+
 app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
 
